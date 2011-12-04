cd79542b3d93e1065628f3e6c7817854cfc17491
Jackfiallos <erling.fiallos@qbit.com.mx>
2011-12-04 03:50:54 -0600

    esta version no tiene conflictos

--- js/hmap.js
+++ js/hmap.js
@@ -20,6 +20,8 @@
 		latitud: '',
 		longitud: '',
 		factorZoom: 0,
+		mapsWidth: 0,
+		mapsHeight: 0,
 		data: {},
 		// Opciones iniciales
 		defaults: {
@@ -44,21 +46,34 @@
 			}
 			else {
 				// Coordenadas de Geolocalizacion
+
 				var center 	= jQuery.googleHeatMaps.obtenerCoords(opts.latitud, opts.longitud);
 				jQuery.googleHeatMaps.gMap.setCenter(center, 16/*opts.factorZoom*/);
+				jQuery.googleHeatMaps.latitud = center.x;
+				jQuery.googleHeatMaps.longitud = center.y;
+
 			}
 			
 			jQuery.googleHeatMaps.gMap.addControl(new GMapTypeControl());
 			jQuery.googleHeatMaps.gMap.addControl(new GLargeMapControl());
 
+
 			GEvent.addListener(jQuery.googleHeatMaps.gMap, "zoomend", function() {
-				jQuery.googleHeatMaps.redibujar();
+				jQuery.googleHeatMaps.redibujar(opts);
 			});
 			
-			label = new ELabel(jQuery.googleHeatMaps.gMap.getCenter(), '<canvas id="carcanvas" width="550" height="450"><\/canvas>',null ,new GSize(-275, 225));
+			
+			/*
+			GEvent.addListener(jQuery.googleHeatMaps.gMap, "move", function() {
+				jQuery.googleHeatMaps.redibujar(opts);
+			});
+			*/
+			
+			
+			label = new ELabel(jQuery.googleHeatMaps.gMap.getCenter(), '<canvas id="carcanvas" width="' + opts.mapsWidth + '" height="' + opts.mapsHeight + '"><\/canvas>',null ,new GSize(-' + (opts.mapsWidth/2) + ', ' + (opts.mapsHeight/2) + '));
 			jQuery.googleHeatMaps.gMap.addOverlay(label);
 			canvas = document.getElementById("carcanvas").getContext('2d');
-			jQuery.googleHeatMaps.redibujar();
+			jQuery.googleHeatMaps.redibujar(opts);
 			// Controles de manejo
 			jQuery.googleHeatMaps.gMap.setUIToDefault();
 			
@@ -99,28 +114,109 @@
 		obtenerCoords: function(latitud, longitud) {
 			return new GLatLng(latitud, longitud);
 		},
-		geoCode: function(options) {
-			geolocalizaDireccion = new GClientGeocoder();
-			geolocalizaDireccion.getLatLng(options.address, function(point) {
-				if (point)
-					jQuery.googleMaps.gMap.setCenter(point, options.depth);
-	      	});
+		filtrardatos: function(data,fechainicio, fechafinal,tipo) {
+		    arraydatos = new Array();
+		    numdatos =0;
+		 	$.each(data, function(key, val) {
+				$.each(val, function(k, v) {				    
+				    if (k=='fecha'){
+				      fecha=v;
+				    }
+				    if (k=='lat'){
+				      latitud=v;
+				    }
+				    if (k=='lon'){
+				      longitud=v;
+				    }
+					if (k=='tipo'){
+						tipodato=v;				
+  					}
+				});
+				
+				if (( ((fecha>=fechainicio)||(fechainicio=='')) && ((fecha<=fechafinal) ||(fechafinal=='')) ) &&
+				    ((tipodato==tipo)||(tipo=='')))
+				{
+					arraydatos[numdatos]= new Array(latitud,longitud);	
+					numdatos = numdatos+1;
+				}
+			});
+			return arraydatos;
 		},
-		redibujar: function () {
+		redibujar: function (opts) {
+			
 			jQuery.googleHeatMaps.clean(canvas);
+			
 			var zoom = jQuery.googleHeatMaps.gMap.getZoom();
 			zoom = Math.pow(2, zoom) / Math.pow(2, 16);
-			canvas.fillStyle = "rgba(250, 0, 0, 0.8)";  
+			canvas.fillStyle = "rgba(0, 0, 200, 0.8)";  
 
-			var y = jQuery.googleHeatMaps.gMap.getCenter().y - 19.406145;
-			var x = jQuery.googleHeatMaps.gMap.getCenter().x - -99.169807;
-			var fac = 47000.0 / (1.0/zoom);
-			canvas.fillRect (275 + (-x*fac), 225 + (y*fac) , 10, 10);
+
+			var a = jQuery.googleHeatMaps.filtrardatos(opts.data,'','30/11/2011','');
+			
+			/* a = new Array();
+			a.push( new Array(19.40637, -99.16998));
+			a.push( new Array(19.40602, -99.17090));
+			a.push( new Array(19.40972, -99.16890)); */  
+			
+			
+			/* for (var i = 0; i < a.length ; i++) { 
+								
+				canvas.fillStyle = "rgba(0, 200, 0, 0.8)";
+				var latlng  = new GLatLng(a[i][0], a[i][1],true);
+				var cor = jQuery.googleHeatMaps.gMap.fromLatLngToContainerPixel(latlng);
+				var y = cor.y;
+				var x = cor.x;
+				canvas.fillRect (x, y , 10, 10);
+			}*/
+			
+			jQuery.googleHeatMaps.pinta(a, canvas);
+			
+			
 		},
-		clean: function (canvas) {
+		pinta: function (a, canvas) 
+		{
+			var pantalla = [];
+			for (var x=0; x<550; x++) {
+				pantalla[x] = new Array();
+				for (var y=0; y<450; y++) {
+					pantalla[x][y] = 0;
+				}
+			}
+			
+			var zoom = jQuery.googleHeatMaps.gMap.getZoom();
+			zoom = Math.pow(2, zoom) / Math.pow(2, 16);
+			canvas.fillStyle = "rgba(250, 0, 0, 0.8)"; 
+			var fac = 47000.0 / (1.0/zoom);
+			
+			for (var i = 0; i < a.length ; i++) {
+				var latlng  = new GLatLng(a[i][0], a[i][1],true);
+				var cor = jQuery.googleHeatMaps.gMap.fromLatLngToContainerPixel(latlng);
+				var y = cor.y;
+				var x = cor.x;
+				// canvas.fillRect (x, y , 10, 10);
+				
+				console.log(x+" "+y);
+				
+				pantalla[parseInt(x)][parseInt(y)] = 1.0;
+				
+			}
+			
+			
+			
+			for (var x=0; x<550; x++) {
+				for (var y=0; y<450; y++) {
+					var value = pantalla[x][y];
+					canvas.fillStyle = "rgba(250, 0, 0, " + value + ")"; 
+					canvas.fillRect (x, y , 20, 20);
+				}
+			}
+		},
+		
+		clean: function (canvas) 
+		{
 			canvas.setTransform(1, 0, 0, 1, 0, 0);
 			canvas.clearRect(0, 0, 
-				jQuery("#carcanvas").width(), jQuery("#carcanvas").height	
+				jQuery("#carcanvas").width(), jQuery("#carcanvas").height()	
 			);
 		},
 		TimeLine: function (startDateName, endDateName, sliderName, buttonName, dateFilteredName, startDate, endDate ) 
@@ -149,9 +245,10 @@
 	                slide: function(event, ui) {TimeLineFilters.onSliderSlide(ui.value);}
 	            });
 	        });
-	    }
+	    }		
 	}
 })(jQuery); 
+
 
 TimeLineFilters =
 {
