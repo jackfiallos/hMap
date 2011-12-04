e48f6bf59a4432f6d05ed5b8e121ab23a106690e
Jackfiallos <erling.fiallos@qbit.com.mx>
2011-12-03 22:40:26 -0600

    Codigo optimizado

--- js/hmap.js
+++ js/hmap.js
@@ -1,23 +1,18 @@
-(function($){
-
-
-	
+(function(jQuery){
 	jQuery.fn.googleHeatMaps = function(options) {
-		if (!window.GBrowserIsCompatible || !GBrowserIsCompatible())  {
+		if (!window.GBrowserIsCompatible || !GBrowserIsCompatible()) {
 		   return this;
 		}
 		// Utilizar defaults si no se establecen opciones
-		var options = $.extend({}, $.googleHeatMaps.defaults, options);
-		
-		
+		var options = jQuery.extend({}, jQuery.googleHeatMaps.defaults, options);		
 		// Crear el mapa
 		return this.each(function() {
-			$.googleHeatMaps.gMap = new GMap2(this, options);
-			$.googleHeatMaps.hmConfiguracion(options);
+			jQuery.googleHeatMaps.gMap = new GMap2(this, options);
+			jQuery.googleHeatMaps.hmConfiguracion(options);
 		});
 	};
 	
-	$.googleHeatMaps = {
+	jQuery.googleHeatMaps = {
 		latitud: '',
 		longitud: '',
 		factorZoom: 0,
@@ -35,45 +30,33 @@
 			if (opts.geoLocalizacion)
 			{
 				geolocalizaDireccion = new GClientGeocoder();
-				geolocalizaDireccion.getLatLng(opts.geoLocalizacion, function(center)
-				{
-					if (center)
-					{
-						$.googleHeatMaps.gMap.setCenter(center, 16/*opts.factorZoom*/);
-						$.googleHeatMaps.latitud = center.x;
-						$.googleHeatMaps.longitud = center.y;
+				geolocalizaDireccion.getLatLng(opts.geoLocalizacion, function(center) {
+					if (center) {
+						jQuery.googleHeatMaps.gMap.setCenter(center, 16/*opts.factorZoom*/);
+						jQuery.googleHeatMaps.latitud = center.x;
+						jQuery.googleHeatMaps.longitud = center.y;
 					}
 	      		});
 			}
 			else {
 				// Coordenadas de Geolocalizacion
-				var center 	= $.googleHeatMaps.obtenerCoords(opts.latitud, opts.longitud);
-				$.googleHeatMaps.gMap.setCenter(center, 16/*opts.factorZoom*/);
+				var center 	= jQuery.googleHeatMaps.obtenerCoords(opts.latitud, opts.longitud);
+				jQuery.googleHeatMaps.gMap.setCenter(center, 16/*opts.factorZoom*/);
 			}
 			
-			
-			
-			$.googleHeatMaps.gMap.addControl(new GMapTypeControl());
-			$.googleHeatMaps.gMap.addControl(new GLargeMapControl());
+			jQuery.googleHeatMaps.gMap.addControl(new GMapTypeControl());
+			jQuery.googleHeatMaps.gMap.addControl(new GLargeMapControl());
 
-			GEvent.addListener($.googleHeatMaps.gMap, "zoomend", function() {
-				$.googleHeatMaps.redibujar();
+			GEvent.addListener(jQuery.googleHeatMaps.gMap, "zoomend", function() {
+				jQuery.googleHeatMaps.redibujar();
 			});
 			
-			label = new ELabel($.googleHeatMaps.gMap.getCenter(), '<canvas id="carcanvas" width="550" height="450"><\/canvas>',null ,new GSize(-275, 225));
-			$.googleHeatMaps.gMap.addOverlay(label);
+			label = new ELabel(jQuery.googleHeatMaps.gMap.getCenter(), '<canvas id="carcanvas" width="550" height="450"><\/canvas>',null ,new GSize(-275, 225));
+			jQuery.googleHeatMaps.gMap.addOverlay(label);
 			canvas = document.getElementById("carcanvas").getContext('2d');
-			$.googleHeatMaps.redibujar();
-			
-			
+			jQuery.googleHeatMaps.redibujar();
 			// Controles de manejo
-			$.googleHeatMaps.gMap.setUIToDefault();
-			// Manipulando los datos
-			/*$.each(opts.data, function(key, val) {
-				$.each(val, function(k, v) {
-					alert(v);
-				});
-			});*/
+			jQuery.googleHeatMaps.gMap.setUIToDefault();
 		},
 		obtenerCoords: function(latitud, longitud) {
 			return new GLatLng(latitud, longitud);
@@ -82,17 +65,13 @@
 			geolocalizaDireccion = new GClientGeocoder();
 			geolocalizaDireccion.getLatLng(options.address, function(point) {
 				if (point)
-					$.googleMaps.gMap.setCenter(point, options.depth);
+					jQuery.googleMaps.gMap.setCenter(point, options.depth);
 	      	});
 		},
-		redibujar: function () 
-		{
-			
-			$.googleHeatMaps.clean(canvas);
-			var zoom = $.googleHeatMaps.gMap.getZoom();
-
+		redibujar: function () {
+			jQuery.googleHeatMaps.clean(canvas);
+			var zoom = jQuery.googleHeatMaps.gMap.getZoom();
 			zoom = Math.pow(2, zoom) / Math.pow(2, 16);
-
 			canvas.fillStyle = "rgba(250, 0, 0, 0.8)";  
 
 			/* data: [
@@ -112,22 +91,16 @@
 			$.each(data, function(clave, valor) {
 				alert(clave.x);
 			}) */
-			
-			var y = $.googleHeatMaps.gMap.getCenter().y - 19.406145;
-			var x = $.googleHeatMaps.gMap.getCenter().x - -99.169807;
-			
+			var y = jQuery.googleHeatMaps.gMap.getCenter().y - 19.406145;
+			var x = jQuery.googleHeatMaps.gMap.getCenter().x - -99.169807;
 			var fac = 47000.0 / (1.0/zoom);
-
 			canvas.fillRect (275 + (-x*fac), 225 + (y*fac) , 10, 10);
-			
 		},
-		clean: function (canvas) 
-		{
+		clean: function (canvas) {
 			canvas.setTransform(1, 0, 0, 1, 0, 0);
 			canvas.clearRect(0, 0, 
-				document.getElementById("carcanvas").width, document.getElementById("carcanvas").height	
+				jQuery("#carcanvas").width(), jQuery("#carcanvas").height	
 			);
-
 		}		
 	}
 })(jQuery); 
